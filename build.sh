d4cf29b096205c3d8b8734596a3c6d0bdeac06cb
hector <super@mercadodejamaica.com>
2026-03-02 23:22:09 -0600

    fix(Vercel): inyectar variables de entorno supabase en .env desde cloud

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 
 # Get dependencies and build web app
-touch .env
+echo "SUPABASE_URL=$SUPABASE_URL" > .env
+echo "SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY" >> .env
 flutter pub get
 flutter build web --release
