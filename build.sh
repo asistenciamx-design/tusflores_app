d4a53fb76932e903a10f5e181e1acd17b421745d
hector <super@mercadodejamaica.com>
2026-03-02 23:05:51 -0600

    fix(Vercel): prevent build crash by mocking missing .env asset

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 
 # Get dependencies and build web app
+touch .env
 flutter pub get
 flutter build web --release
